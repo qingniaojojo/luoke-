633e839a89e590e3e818d5a5f8cb2a942ca27075
qingniaojojo <2066893518@qq.com>
2026-07-03 20:16:48 +0800

    feat(data-index): 新增数据首页页面骨架
    新增了数据首页的基础页面文件，包含自定义头部组件占位

---
+++ pages/data/data-index.vue
@@ -0,0 +1,17 @@
+<template>
+	<view class="dataindex">
+		<constom-head-top>
+			<template #left>
+				
+			</template>
+			<template #right></template>
+			
+		</constom-head-top>
+	</view>
+</template>
+
+<script setup>
+</script>
+
+<style lang="scss" scoped>
+</style>
